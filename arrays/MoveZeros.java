93839085b62f36c8769523b059151a9e2f819ed3
Ritika-23-2007 <ritika.agrwl.2007@gmail.com>
2026-07-12 11:23:57 +0530

    move zeros to end

---
+++ arrays/MoveZeros.java
@@ -0,0 +1,32 @@
+package arrays;
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//Note that you must do this in-place without making a copy of the array.
+
+//Example 1:
+//Input: nums = [0,1,0,3,12]
+//Output: [1,3,12,0,0]
+
+//Example 2:
+//Input: nums = [0]
+//Output: [0]
+ 
+//Constraints:
+//1 <= nums.length <= 104
+//-231 <= nums[i] <= 231 - 1
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int temp;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length-1; j++) {
+                if (nums[j] == 0 && nums[j+1] != 0) {
+                    temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+//time complexity O(n^2)
+//space complexity O(1)        
+    }
+}
